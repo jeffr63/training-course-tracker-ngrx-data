da81a722fb9cfa31a3908c9e6540b8e39140f2eb
jeffr63 <jeffr63@gmail.com>
2022-01-13 11:33:05 -0600

    Added sortComparer to path and source metadata

--- src/app/entity-metadata.ts
+++ src/app/entity-metadata.ts
@@ -1,9 +1,17 @@
 import { EntityMetadataMap } from '@ngrx/data';
 
 const entityMetadata: EntityMetadataMap = {
-  Paths: {},
-  Sources: {},
+  Paths: {
+    sortComparer: sortByName,
+  },
+  Sources: {
+    sortComparer: sortByName,
+  },
 };
+
+export function sortByName(a: { name: string }, b: { name: string }): number {
+  return a.name.localeCompare(b.name);
+}
 
 const pluralNames = { Paths: 'Paths', Sources: 'Sources' };
 
