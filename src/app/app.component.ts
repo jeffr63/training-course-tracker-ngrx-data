49f7bd8cf3f95a3b0d6fde75d54dadb4465ba0ee
jeffr63 <jeffr63@gmail.com>
2022-08-01 11:31:49 -0500

    Trying alternative boostrap process from Angular Air

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Component, enableProdMode, importProvidersFrom, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, TitleStrategy } from '@angular/router';
 
+import { DefaultDataServiceConfig, EntityDataModule } from '@ngrx/data';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { environment } from 'src/environments/environment';
+import { entityConfig } from './entity-metadata';
+import { APP_ROUTES } from './app.routes';
 import { AuthService } from './auth/auth.service';
+import { CustomTitleStrategyService } from './services/custom-title-strategy.service';
 import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    MenuComponent, RouterModule
-  ],
+  imports: [MenuComponent, RouterModule],
   template: `
-  <app-menu></app-menu>
+    <app-menu></app-menu>
     <main>
       <router-outlet></router-outlet>
     </main>
@@ -25,5 +35,36 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.authService.checkLogin();
+  }
+
+  public static bootstrap() {
+    // alternative bootstrap method discussed on Angular Air.  This moves all of the boilerplate out of main.ts and moved to bootstraped component.
+    const defaultDataServiceConfig: DefaultDataServiceConfig = {
+      root: 'http://localhost:3000',
+      timeout: 3000, // request timeout
+    };
+
+    if (environment.production) {
+      enableProdMode();
+    }
+
+    bootstrapApplication(this, {
+      providers: [
+        importProvidersFrom(
+          BrowserAnimationsModule,
+          HttpClientModule,
+          StoreModule.forRoot({}),
+          EffectsModule.forRoot([]),
+          EntityDataModule.forRoot(entityConfig),
+          StoreDevtoolsModule.instrument({
+            maxAge: 5,
+            logOnly: environment.production,
+          }),
+          RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy' })
+        ),
+        { provide: DefaultDataServiceConfig, useValue: defaultDataServiceConfig },
+        { provide: TitleStrategy, useClass: CustomTitleStrategyService },
+      ],
+    }).catch((err) => console.error(err));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { entityConfig } from './app/entity-metadata';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, TitleStrategy } from '@angular/router';
-
-import { DefaultDataServiceConfig, EntityDataModule } from '@ngrx/data';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { CustomTitleStrategyService } from './app/services/custom-title-strategy.service';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-const defaultDataServiceConfig: DefaultDataServiceConfig = {
-  root: 'http://localhost:3000',
-  timeout: 3000, // request timeout
-};
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      BrowserAnimationsModule,
-      HttpClientModule,
-      StoreModule.forRoot({}),
-      EffectsModule.forRoot([]),
-      EntityDataModule.forRoot(entityConfig),
-      StoreDevtoolsModule.instrument({
-        maxAge: 5,
-        logOnly: environment.production,
-      }),
-      RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy' })
-    ),
-    { provide: DefaultDataServiceConfig, useValue: defaultDataServiceConfig },
-    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
-  ],
-}).catch((err) => console.error(err));
-
+// alternative bootstrap method discussed on Angular Air.  This moves all of the boilerplate out of main.ts and moved to bootstraped component.
+AppComponent.bootstrap();
