7bc1391e30cf3ecdceb8182b74c4580c6b07ee42
Jeff Richardson <jeffr63@gmail.com>
2025-03-10 10:31:35 -0500

    Created source edit card presentational component

--- src/app/admin/source/source-edit.component.ts
+++ src/app/admin/source/source-edit.component.ts
@@ -1,63 +1,24 @@
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { Component, OnInit, inject, DestroyRef, input } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { Source } from '@models/sources';
+import { SourceEditCardComponent } from './source-edit-card.component';
 import { SourceService } from '@shared/services/source/source.service';
 
 @Component({
   selector: 'app-source-edit',
-  imports: [NgbModule, ReactiveFormsModule, RouterLink],
-  template: `
-    <section class="container">
-      <section class="card">
-        @if (sourceEditForm) {
-        <form [formGroup]="sourceEditForm">
-          <fieldset class="m-2 row">
-            <label class="col-form-label col-sm-2" for="name">Source Name</label>
-            <div class="col-sm-6">
-              <input type="text" class="form-control" formControlName="name" placeholder="Enter source name" />
-              @if (sourceEditForm.controls.name.errors?.required && sourceEditForm.controls.name?.touched) {
-              <small class="text-danger">Source Name is required</small>
-              }
-            </div>
-          </fieldset>
-
-          <div class="d-grid gap-2 m-2 d-sm-flex justify-content-sm-end">
-            <button class="btn btn-primary" (click)="save()" title="Save" [disabled]="!sourceEditForm.valid">
-              <i class="bi bi-save"></i> Save
-            </button>
-            <a class="btn btn-secondary" [routerLink]="['/admin/sources']" title="Cancel">
-              <i class="bi bi-x-circle"></i> Cancel
-            </a>
-          </div>
-        </form>
-        }
-      </section>
-    </section>
-  `,
-  styles: [
-    `
-      section .card {
-        margin-top: 30px;
-        padding-left: 15px;
-        padding-right: 15px;
-      }
-      .form-buttons {
-        margin-left: 3px;
-      }
-    `,
-  ],
+  imports: [SourceEditCardComponent],
+  template: `<app-source-edit-card [(sourceEditForm)]="sourceEditForm" (cancel)="cancel()" (save)="save()" />`,
 })
 export default class SourceEditComponent implements OnInit {
   readonly #fb = inject(FormBuilder);
   readonly #location = inject(Location);
   readonly #sourceService = inject(SourceService);
   readonly #destroyRef = inject(DestroyRef);
+  readonly #router = inject(Router);
 
   protected readonly id = input.required<string>();
   #isNew = true;
@@ -79,6 +40,10 @@ export default class SourceEditComponent implements OnInit {
         this.#source = { ...source };
         this.sourceEditForm.get('name').setValue(this.#source.name);
       });
+  }
+
+  protected cancel() {
+    this.#router.navigate(['/admin/sources']);
   }
 
   protected save() {
